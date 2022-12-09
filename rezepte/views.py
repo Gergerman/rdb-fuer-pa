3290899b7beb4e30ce6a1c0f8395eefd652aa4b1
Gerald <gerald.deutsch@posteo.at>
2022-12-09 16:32:59 +0100

    Listview hinzu

--- rezepte/views.py
+++ rezepte/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Rezept
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+class RezeptListView(ListView):
+    model = Rezept
+    template_name = 'list.html'
