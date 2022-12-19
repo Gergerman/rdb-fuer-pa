b12da038499ebdc1b892d66d4b42986f100addc4
Gerald <gerald.deutsch@posteo.at>
2022-12-19 21:06:41 +0100

    Model Speisenart hinzu

--- rezepte/admin.py
+++ rezepte/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Rezept
+from .models import Rezept, Speisenart
 
 #class RezeptAdmin(admin.ModelAdmin):
 #    list_display = ("bezeichnung", "koch", "kategorie",)
 
 admin.site.register(Rezept)
+admin.site.register(Speisenart)
--- rezepte/models.py
+++ rezepte/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+class Speisenart(models.Model):
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        verbose_name_plural = "Speisenarten"
+
+    def __str__(self):
+        return self.name
+
 class Rezept(models.Model):
     koch = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     bezeichnung = models.CharField(max_length=80)
