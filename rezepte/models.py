3064dc27a8d7b4e20948d1fac0f6307227395af8
Gerald <gerald.deutsch@posteo.at>
2024-08-05 20:47:30 +0200

    vietnamesisch

--- rezepte/models.py
+++ rezepte/models.py
@@ -54,6 +54,7 @@ KUECHE = [
     ('japanisch','japanisch'),
     ('georgisch','georgisch'),
     ('levantinisch','levantinisch'),
+    ('vietnamesisch','vietnamesisch'),
     ('---------','---------')
     ]
 
